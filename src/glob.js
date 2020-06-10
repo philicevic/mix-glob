7084f94c3823302e0dec117d1f319fc8e331275b
Philipp Ehrenberg <ehrenberg.ph@gmail.com>
2020-06-10 14:31:06 +0200

    Support relative output paths

--- src/glob.js
+++ src/glob.js
@@ -1,7 +1,13 @@
+let globParent = require('glob-parent');
+let path = require('path');
+
 class Glob {
     register(component, parameters = []) {
-        let files;
-        let supportedComponents = [
+        let files,
+            fileRoot,
+            outputFile,
+            params,
+            supportedComponents = [
           'sass',
           'less',
           'stylus',
@@ -12,9 +18,19 @@ class Glob {
         ];
         if (supportedComponents.includes(component)) {
             files = glob.sync(parameters[0]);
+            fileRoot = globParent(parameters[0]);
             files.forEach(file => {
-                parameters[0] = file;
-                mix[component](...parameters);
+                params = [...parameters];
+
+                // set entry file
+                params[0] = file;
+
+                // build relative output path
+                outputFile = path.normalize(parameters[1]) + path.sep + path.relative(fileRoot, file);
+                params[1] = path.dirname(outputFile);
+
+                // run component
+                mix[component](...params);
             });
         }
         else {
